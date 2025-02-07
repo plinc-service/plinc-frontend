727d14c0008ac9c8b630de160ec5778bce9e1ec4
Anani Serge AMOUSSOUGBO <amoussougboserge@gmail.com>
2025-02-07 22:07:52 +0100

    fix(UserInterface): resolve merge conflicts and clean up Service interface definition

--- src/interfaces/userInterface.ts
+++ src/interfaces/userInterface.ts
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 import { Service } from "./serviceInterface";
-=======
-export interface Service {
-  id: number;
-  title: string;
-  description: string;
-  enabled: boolean;
-}
->>>>>>> 1bea3d25a5cbade29a96b40ac000d88fb8c3ff1a
 
 export interface User {
   id: string;
@@ -23,15 +14,12 @@ export interface User {
   date_joined: string;
   is_active: boolean;
   services: Service[];
-<<<<<<< HEAD
-=======
   siret_num?: string;
   iban_num?: string;
   number_plinc_buyer: number;
   number_plinc_seller: number;
   revenue_waiting: number;
   revenue_total: number;
->>>>>>> 1bea3d25a5cbade29a96b40ac000d88fb8c3ff1a
 }
 
 export interface UserResponse {
